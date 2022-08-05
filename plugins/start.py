e6dc8618ef0fdfe6632ec873ef9c47bc8009edc6
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-08-05 09:45:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -34,12 +34,6 @@ from helper.utils import not_subscribed
 FLOOD = int(environ.get("FLOOD", "10"))
 START_PIC = environ.get("START_PIC", "https://telegra.ph/file/04d08445dce68c9a57b25.jpg")
 
-@Client.on_message(filters.private & filters.create(not_subscribed))
-async def is_not_subscribed(client, message):
-    buttons = [[ InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=client.invitelink) ]]
-    text = "**𝚂𝙾𝚁𝚁𝚈 𝙳𝚄𝙳𝙴 𝚈𝙾𝚄𝚁 𝙽𝙾𝚃 𝙹𝙾𝙸𝙽𝙳 𝙼𝚈 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 😔. 𝙿𝙻𝙴𝙰𝚂𝙴 𝙹𝙾𝙸𝙽 𝙼𝚈 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝚃𝙾 𝚄𝚂𝙴 𝚃𝙷𝙸𝚂 𝙱𝙾𝚃 🙏 **"
-    await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-           
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client, message):
     insert(int(message.chat.id))
