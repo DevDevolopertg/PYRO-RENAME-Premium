16bde92c349c10c1f2b6a07680a70cab60956b47
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-08-05 09:49:14 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from helper.database import find, delthumb, addthumb
 from helper.txt import mr, AUTH_USERS
 
-@Client.on_message(filters.private & filters.command(['viewthumb']))
+@Client.on_message(filters.private & filters.command(['viewthumb']) & filters.user(AUTH_USERS))
 async def viewthumb(client,message):
     thumb = find(int(message.chat.id))[0]
     if thumb:
@@ -12,12 +12,12 @@ async def viewthumb(client,message):
     else:
         await message.reply_text("**You dont have any custom Thumbnail**") 
 		
-@Client.on_message(filters.private & filters.command(['delthumb']))
+@Client.on_message(filters.private & filters.command(['delthumb']) & filters.user(AUTH_USERS))
 async def removethumb(client,message):
     delthumb(int(message.chat.id))
     await message.reply_text("**Custom Thumbnail Deleted Successfully**")
 	
-@Client.on_message(filters.private & filters.photo)
+@Client.on_message(filters.private & filters.photo & filters.user(AUTH_USERS))
 async def addthumbs(client,message):
     file_id = str(message.photo.file_id)
     addthumb(message.chat.id , file_id)
