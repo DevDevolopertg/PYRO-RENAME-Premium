bfae005e77a2ffb1a017dc73a9fdcd13fefdce42
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-08-05 10:13:32 +0530

    Update user.py

--- user.py
+++ user.py
@@ -4,6 +4,7 @@ import asyncio
 import logging
 
 PREMIUM_SESSION = os.environ.get("PREMIUM_SESSION", "pyrogram session")              
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "Rename_bot")
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
@@ -17,5 +18,16 @@ class User(Client):
             workers=50
         )
         self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        if BOT_USERNAME:
+            await User.send_message(self, chat_id=BOT_USERNAME, text="🚀 BOT WORK STARTED ❤️‍🔥")
+        usr_bot_me = await self.get_me()
+        return (self, usr_bot_me.id)
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped. Bye.")
 
     
